865715375d05a581ff487d2c7736cc7616bdedbd
farhan-hasan <syedfarhan6491232@gmail,com>
2024-06-22 12:50:23 +0600

    register UI done!

--- app/src/main/java/com/example/messmate/RegisterActivity.java
+++ app/src/main/java/com/example/messmate/RegisterActivity.java
@@ -3,31 +3,23 @@ package com.example.messmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        Button registerButton = findViewById(R.id.loginActivityRegisterButton);
+        Button registerButton = findViewById(R.id.registerActivityLoginButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to start the new activity
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
