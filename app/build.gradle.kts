e7f5f80953eaa4094f71f800e3587f2a470d2c93
farhan-hasan <syedfarhan6491232@gmail,com>
2024-06-27 19:42:48 +0600

    pc to laptop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.6.0")
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation("com.google.firebase:firebase-auth:23.0.0")// or latest version
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")// or latest version
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
