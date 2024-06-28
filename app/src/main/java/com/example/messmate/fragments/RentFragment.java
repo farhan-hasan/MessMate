e2851303f4a6caabcd345c23dd0f68c049d71137
farhan-hasan <syedfarhan6491232@gmail,com>
2024-06-28 20:14:30 +0600

    Mess list retrieve done in rent fragment, search fragment and meal management activity

--- app/src/main/java/com/example/messmate/fragments/RentFragment.java
+++ app/src/main/java/com/example/messmate/fragments/RentFragment.java
@@ -14,17 +14,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.messmate.R;
 import com.example.messmate.adapters.MessListRecyclerAdapter;
 import com.example.messmate.models.Constants;
+import com.example.messmate.models.MessDetailsModel;
 import com.example.messmate.models.MessListCardModel;
 import com.example.messmate.screens.HomeActivity;
 import com.example.messmate.screens.RentMessDetailsActivity;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.orhanobut.dialogplus.DialogPlus;
 
 import java.util.ArrayList;
@@ -47,124 +55,165 @@ public class RentFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_rent, container, false);
-
         addButton = view.findViewById(R.id.addMessButton);
-
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final DialogPlus dialogPlus = DialogPlus.newDialog(requireContext())
-                        .setContentHolder(new com.orhanobut.dialogplus.ViewHolder(R.layout.add_mess_popup))
-                        .setExpanded(true, 1400)
-                        .create();
+                addMess();
+            }
+        });
+        loadFragment();
+        // Inflate the layout for this fragment
+        return view;
+    }
 
-                //dialogPlus.show();
+    public void addMess() {
+        final DialogPlus dialogPlus = DialogPlus.newDialog(requireContext())
+                .setContentHolder(new com.orhanobut.dialogplus.ViewHolder(R.layout.add_mess_popup))
+                .setExpanded(true, 1400)
+                .create();
 
-                View popupView = dialogPlus.getHolderView();
-                EditText messNameEditText = popupView.findViewById(R.id.messNameEditText);
-                EditText messAddressEditText = popupView.findViewById(R.id.messAddressEditText);
-                EditText numberOfSeatsEditText = popupView.findViewById(R.id.numberOfSeatsEditText);
-                EditText rentPerSeatEditText = popupView.findViewById(R.id.rentPerSeatEditText);
-                Button poppupAddButton = popupView.findViewById(R.id.messAddButton);
+        //dialogPlus.show();
+
+        View popupView = dialogPlus.getHolderView();
+        EditText messNameEditText = popupView.findViewById(R.id.messNameEditText);
+        EditText messAddressEditText = popupView.findViewById(R.id.messAddressEditText);
+        EditText numberOfSeatsEditText = popupView.findViewById(R.id.numberOfSeatsEditText);
+        EditText rentPerSeatEditText = popupView.findViewById(R.id.rentPerSeatEditText);
+        Button poppupAddButton = popupView.findViewById(R.id.messAddButton);
 
 
 
-                dialogPlus.show();
-                poppupAddButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+        dialogPlus.show();
+        poppupAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
 
-                        Map<String,Object> messDetails = new HashMap<>();
-                        Map<String,Object> breakFastDetails = new HashMap<>();
-                        Map<String,Object> lunchDetails = new HashMap<>();
-                        Map<String,Object> dinnerDetails = new HashMap<>();
-                        Map<String,Object> mealRequest = new HashMap<>();
-                        if (messNameEditText.getText()!=null && messAddressEditText.getText()!=null &&
-                                numberOfSeatsEditText.getText()!=null && rentPerSeatEditText.getText()!=null) {
+                Map<String,Object> messDetails = new HashMap<>();
+                Map<String,Object> breakFastDetails = new HashMap<>();
+                Map<String,Object> lunchDetails = new HashMap<>();
+                Map<String,Object> dinnerDetails = new HashMap<>();
+                Map<String,Object> mealRequest = new HashMap<>();
+                if (messNameEditText.getText()!=null && messAddressEditText.getText()!=null &&
+                        numberOfSeatsEditText.getText()!=null && rentPerSeatEditText.getText()!=null) {
 
-                            String messName = messNameEditText.getText().toString().trim();
-                            String messAddress = messAddressEditText.getText().toString().trim();
-                            int numberOfSeats = Integer.parseInt(numberOfSeatsEditText.getText().toString().trim());
-                            int rentPerSeat = Integer.parseInt(rentPerSeatEditText.getText().toString().trim());
+                    String messName = messNameEditText.getText().toString().trim();
+                    String messAddress = messAddressEditText.getText().toString().trim();
+                    int numberOfSeats = Integer.parseInt(numberOfSeatsEditText.getText().toString().trim());
+                    int rentPerSeat = Integer.parseInt(rentPerSeatEditText.getText().toString().trim());
+                    final String[] adminPhone = new String[1];
 
-                            messDetails.put("mess_name", messName);
-                            messDetails.put("mess_address", messAddress);
-                            messDetails.put("number_of_seats", numberOfSeats);
-                            messDetails.put("rent_per_seat", rentPerSeat);
-                            messDetails.put("admin", Constants.userKey);
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(Constants.userKey).child("phone");
 
-                            breakFastDetails.put("menu", "");
-                            breakFastDetails.put("price", 0);
-                            lunchDetails.put("menu", "");
-                            lunchDetails.put("price", 0);
-                            dinnerDetails.put("menu", "");
-                            dinnerDetails.put("price", 0);
-                            mealRequest.put("breakfast", 0);
-                            mealRequest.put("lunch", 0);
-                            mealRequest.put("dinner", 0);
-
-                            messDetails.put("breakfast", breakFastDetails);
-                            messDetails.put("lunch", lunchDetails);
-                            messDetails.put("dinner", dinnerDetails);
-                            messDetails.put("meal_request", mealRequest);
-                            messDetails.put("residents", "");
-
-                            String messKey = messName.toLowerCase() + "_" + messAddress.toLowerCase();
-
-                            FirebaseDatabase.getInstance().getReference().child("Messes")
-                                    .child(messKey.replace(" ","")).setValue(messDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            Toast.makeText(requireContext(), "Mess added successfully", Toast.LENGTH_SHORT).show();
-                                            dialogPlus.dismiss();
-                                        }
-                                    }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(requireContext(), "Failed to add mess data", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-
-                        } else {
-                            Toast.makeText(requireContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot
+                                    .exists()) {
+                                adminPhone[0] = snapshot.getValue(String.class);
+                            }
                         }
-                    }
-                });
 
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(requireContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    messDetails.put("mess_name", messName);
+                    messDetails.put("mess_address", messAddress);
+                    messDetails.put("number_of_seats", numberOfSeats);
+                    messDetails.put("available_seats", numberOfSeats);
+                    messDetails.put("rent_per_seat", rentPerSeat);
+                    messDetails.put("admin", Constants.userKey);
+                    messDetails.put("admin_phone", adminPhone[0]);
+
+                    breakFastDetails.put("menu", "");
+                    breakFastDetails.put("price", 0);
+                    lunchDetails.put("menu", "");
+                    lunchDetails.put("price", 0);
+                    dinnerDetails.put("menu", "");
+                    dinnerDetails.put("price", 0);
+                    mealRequest.put("breakfast", 0);
+                    mealRequest.put("lunch", 0);
+                    mealRequest.put("dinner", 0);
+
+                    messDetails.put("breakfast", breakFastDetails);
+                    messDetails.put("lunch", lunchDetails);
+                    messDetails.put("dinner", dinnerDetails);
+                    messDetails.put("meal_request", mealRequest);
+                    messDetails.put("residents", "");
+
+                    String messKey = messName.toLowerCase() + "_" + messAddress.toLowerCase();
+
+                    FirebaseDatabase.getInstance().getReference().child("Messes")
+                            .child(messKey.replace(" ","")).setValue(messDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(requireContext(), "Mess added successfully", Toast.LENGTH_SHORT).show();
+                                    dialogPlus.dismiss();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(requireContext(), "Failed to add mess data", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                } else {
+                    Toast.makeText(requireContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        loadFragment();
-
-        // Inflate the layout for this fragment
-        return view;
     }
 
     public void loadFragment() {
 
         recyclerView = view.findViewById(R.id.rentMessListRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        messListCardItems.add(new MessListCardModel("10", "My mess 1", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 2", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 3", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 4", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 5", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 6", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 7", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 8", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 9", "Sylhet", "+8801723232323"));
-        messListCardItems.add(new MessListCardModel("10", "My mess 10", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 1", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 2", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 3", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 4", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 5", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 6", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 7", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 8", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 9", "Sylhet", "+8801723232323"));
+//        messListCardItems.add(new MessListCardModel("10", "My mess 10", "Sylhet", "+8801723232323"));
 
-        messListRecyclerAdapter = new MessListRecyclerAdapter(getActivity(), messListCardItems, new MessListRecyclerAdapter.OnItemClickListener() {
+        DatabaseReference messesRef = FirebaseDatabase.getInstance().getReference().child("Messes");
+        Query query = messesRef.orderByChild("admin").equalTo(Constants.userKey);
+
+        FirebaseRecyclerOptions<MessDetailsModel> options =
+                new FirebaseRecyclerOptions.Builder<MessDetailsModel>()
+                        .setQuery(query, MessDetailsModel.class).build();
+        messListRecyclerAdapter = new MessListRecyclerAdapter(getActivity(), new MessListRecyclerAdapter.OnItemClickListener() {
 
             @Override
-            public void onItemClick(MessListCardModel item) {
-                String message = "Clicked " + item.messName + " ";
+            public void onItemClick(MessDetailsModel item) {
+                String message = "Clicked " + item.mess_name + " ";
                 startActivity(new Intent(getActivity(), RentMessDetailsActivity.class));
                 Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
             }
-        });
+        }, options);
         recyclerView.setAdapter(messListRecyclerAdapter);
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (messListRecyclerAdapter != null) {
+            messListRecyclerAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (messListRecyclerAdapter != null) {
+            messListRecyclerAdapter.stopListening();
+        }
     }
 }
